934f9b114fd030176ca88cd59438abbebdd01ec7
HariShrestha <hshresth@msudenver.edu>
2019-02-24 11:43:41 -0700

    truck example

--- main.c
+++ main.c
@@ -23,13 +23,19 @@ void wont_change(int *argument_i) {
 void change_inside(int *pointer_to_global_i) {
     *pointer_to_global_i = 10; // dereference and assign a new value
 }
-int now_changed(int num){
+int now_changed(int num){ // adding by 1
     return num + 1;
 }
 
 void changed( int *dum){
     *dum = 7;
 }
+struct student_record {
+    char first_name[5];      // these are called members or fields
+    char last_name[5];       // they can be of any type
+    char nine_hundred[5];
+    int year_first_enrolled;
+};
 
 
 
@@ -41,6 +47,7 @@ int main() {
     printf("%d\n", global_i);
     change_inside(&global_i);
     printf("%d\n", global_i);
+
     global_i = now_changed(global_i);
     printf("%d\n", global_i);
     changed(&global_i);
@@ -64,6 +71,27 @@ int main() {
 
         printf("%c\n", array[i]);
     }
+    int ray[] = {5,10,89,3,50}; // size of gives the size of bytes
+    int ray_size = (sizeof(ray) / sizeof(ray[0])); // sizeof(ray) gives total bytes and sizeof(ray[0]) gives bytes of
+                                                   // one
+    printf("size of ray %d\n", ray_size);
+
+    int reey[50];
+    reey[5] = 10;
+    printf("5th index is %d \n", reey[5]);
+
+
+    // struck
+    struct student_record jm123 = { "john", "michaels", "900123456", 1994 };
+    printf("%s-%s-%s-%d\n", jm123.first_name,
+           jm123.last_name,
+           jm123.nine_hundred,
+           jm123.year_first_enrolled);
+    struct student_record *student_pointer = &jm123;
+    printf("%s-%s-%s-%d\n", student_pointer->first_name,
+           student_pointer->last_name,
+           student_pointer->nine_hundred,
+           student_pointer->year_first_enrolled);
 
 
 
